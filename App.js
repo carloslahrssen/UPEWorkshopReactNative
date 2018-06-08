09712a0db2c16dca081a7a57ec7ed451b663eef7
Ryan Craig Martin <rmart106@fiu.edu>
2018-06-08 18:40:56 -0400

    Fix: Commented out createBottomTabNavigator in NavigateTodo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Container, Root} from 'native-base';
+import { Container, Root } from 'native-base';
 import { Font, AppLoading } from "expo";
 
 /*Components Go Here*/
@@ -12,83 +12,82 @@ import NavigateTodo from './components/NavigateTodo';
 import _ from 'lodash';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       items: [
-        {id:1, title: 'First', active:true },
-        {id:2, title: 'Second', active:false}
+        { id: 1, title: 'First', active: true },
+        { id: 2, title: 'Second', active: false }
       ],
-      itemFilter:'active',
-      loading:true,
+      itemFilter: 'active',
+      loading: true,
     };
-   
+
   }
 
   async componentWillMount() {
     await Font.loadAsync({
-    Roboto: require("native-base/Fonts/Roboto.ttf"),
-    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
     });
     this.setState({ loading: false });
   }
 
   addTodoCallback = (childVar) => {
     let newId = 1;
-    
-    if(this.state.items.length > 0){
+
+    if (this.state.items.length > 0) {
       newId = _.maxBy(this.state.items, 'id').id + 1;
     } else {
       newId = 1;
     }
 
-    this.state.items.push({id:newId,title:childVar, active: true});
+    this.state.items.push({ id: newId, title: childVar, active: true });
     this.setState(this.state);
   }
 
   navigationCallback = (value) => {
-    this.setState({itemFilter:value});
+    this.setState({ itemFilter: value });
   }
 
   changeStatus = (itemId) => {
     let items = this.state.items;
-    let index = _.findIndex(items, {'id':itemId});
+    let index = _.findIndex(items, { 'id': itemId });
 
-    if(items[index].active){
+    if (items[index].active) {
       items[index].active = false;
     } else {
       items[index].active = true;
     }
 
-    this.setState({items});
+    this.setState({ items });
   }
 
   deleteItem = (itemId) => {
     let items = this.state.items;
-    let index = _.findIndex(items, {id: itemId});
-    
+    let index = _.findIndex(items, { id: itemId });
+
     _.pullAt(items, [index]);
-    
-    this.setState({items});
+
+    this.setState({ items });
   }
 
   render() {
-    if(this.state.loading)
-    {
+    if (this.state.loading) {
       return (
         <Root>
           <AppLoading />
         </Root>
       );
     }
-    else{
+    else {
       return (
         <Container>
-          <AddTodo placeholder="I, a prop" filter={this.state.itemFilter} callbackAddInput={this.addTodoCallback}/>
+          <AddTodo placeholder="I, a prop" filter={this.state.itemFilter} callbackAddInput={this.addTodoCallback} />
           {/* Display Todos go here */}
-          <TodoList items={this.state.items} changeStatus={this.changeStatus} deleteItem={this.deleteItem} filter={this.state.itemFilter}/>
+          <TodoList items={this.state.items} changeStatus={this.changeStatus} deleteItem={this.deleteItem} filter={this.state.itemFilter} />
           {/* Navigate between active and inactive */}
-          <NavigateTodo  filter={this.state.itemFilter} callbackNavigation={this.navigationCallback}/>
+          <NavigateTodo filter={this.state.itemFilter} callbackNavigation={this.navigationCallback} />
         </Container>
       );
     }
--- components/NavigateTodo.js
+++ components/NavigateTodo.js
@@ -1,41 +1,41 @@
-import React, {Component} from 'react';
-import {createBottomTabNavigator} from 'react-navigation';
+import React, { Component } from 'react';
 import { Footer, FooterTab, Button, Text } from 'native-base';
+// import { createBottomTabNavigator } from 'react-navigation';
 
-export default class NavigateTodo extends Component{
-   constructor(props){
-      super(props);
-      this.state={
-         activeButton: 'active',
-      }
-   }
+export default class NavigateTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeButton: 'active',
+    }
+  }
 
-   navigateCallback = (value) => {
-      this.setState({activeButton:value});
-      this.props.callbackNavigation(value);
-   }
+  navigateCallback = (value) => {
+    this.setState({ activeButton: value });
+    this.props.callbackNavigation(value);
+  }
 
-   render(){
-      return (
-         <Footer>
-            <FooterTab>
+  render() {
+    return (
+      <Footer>
+        <FooterTab>
 
-               <Button 
-                  active={this.state.activeButton === 'active'}
-                  onPress={() => (this.navigateCallback('active'))} 
-                  >
-                  <Text>To Do</Text>
-               </Button>
-            </FooterTab>
+          <Button
+            active={this.state.activeButton === 'active'}
+            onPress={() => (this.navigateCallback('active'))}
+          >
+            <Text>To Do</Text>
+          </Button>
+        </FooterTab>
 
-            <FooterTab>
-               <Button 
-                  active={this.state.activeButton === 'all'}
-                  onPress={() => (this.navigateCallback('all'))}>
-                  <Text>Done</Text>
-               </Button>
-            </FooterTab>
-         </Footer>
-      );
-   }   
+        <FooterTab>
+          <Button
+            active={this.state.activeButton === 'all'}
+            onPress={() => (this.navigateCallback('all'))}>
+            <Text>Done</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    );
+  }
 }
